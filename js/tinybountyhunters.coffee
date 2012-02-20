8514910d55ffb8508fd53b42823eb94487b72044
Matt Gauger <matt.gauger@gmail.com>
2012-02-19 19:52:54 -0600

    game menu, play states

--- js/tinybountyhunters.coffee
+++ js/tinybountyhunters.coffee
@@ -2,15 +2,22 @@ fps = $('#fps')
 
 menuGameState = ->
   setup: ->
+    jaws.on_keydown ['enter', 'space'], -> jaws.switchGameState(playGameState)
     console.log 'Setup!'
-  update: ->
   draw: ->
+    jaws.context.clearRect 0,0, jaws.width, jaws.height
+    jaws.context.font = 'bold 50pt Terminal'
+    jaws.context.fillStyle = "Black"
+    jaws.context.strokeStyle = "rgba(200,200,200,0.0)"
+    jaws.context.fillText 'Start', 30, 160
 
 playGameState = ->
   setup: ->
     console.log 'Setup!'
   update: ->
+    fps.text jaws.game_loop.fps
   draw: ->
+    jaws.context.clearRect 0,0, jaws.width, jaws.height
 
 $ ->
   jaws.start(menuGameState)
--- js/tinybountyhunters.js
+++ js/tinybountyhunters.js
@@ -6,10 +6,18 @@
   menuGameState = function() {
     return {
       setup: function() {
+        jaws.on_keydown(['enter', 'space'], function() {
+          return jaws.switchGameState(playGameState);
+        });
         return console.log('Setup!');
       },
-      update: function() {},
-      draw: function() {}
+      draw: function() {
+        jaws.context.clearRect(0, 0, jaws.width, jaws.height);
+        jaws.context.font = 'bold 50pt Terminal';
+        jaws.context.fillStyle = "Black";
+        jaws.context.strokeStyle = "rgba(200,200,200,0.0)";
+        return jaws.context.fillText('Start', 30, 160);
+      }
     };
   };
 
@@ -18,8 +26,12 @@
       setup: function() {
         return console.log('Setup!');
       },
-      update: function() {},
-      draw: function() {}
+      update: function() {
+        return fps.text(jaws.game_loop.fps);
+      },
+      draw: function() {
+        return jaws.context.clearRect(0, 0, jaws.width, jaws.height);
+      }
     };
   };
 
