03e68df10ca0de03aff17fa2d4b786e557bef371
Matt Gauger <matt.gauger@gmail.com>
2012-03-02 18:31:35 -0600

    Implement basic player plus physics

--- js/tinybountyhunters.coffee
+++ js/tinybountyhunters.coffee
@@ -3,23 +3,34 @@ fps = $('#fps')
 menuGameState = ->
   setup: ->
     jaws.on_keydown ['enter', 'space'], -> jaws.switchGameState(playGameState)
+    return
   draw: ->
     jaws.context.clearRect 0,0, jaws.width, jaws.height
     jaws.context.font = 'bold 50pt Terminal'
     jaws.context.fillStyle = "Black"
     jaws.context.strokeStyle = "rgba(200,200,200,0.0)"
     jaws.context.fillText 'Start', 30, 160
+    return
 
 playGameState = ->
+  player = new jaws.Sprite(
+    x: 110
+    y: 64
+    scale: 2
+    anchor: 'center_bottom'
+  )
   setup: ->
+    newblock = (x,y) ->
+      blocks.push(new jaws.Sprite(image: 'img/block.png', x: x, y: y))
+
     window.blocks = new jaws.SpriteList()
-    window.world = jaws.Rect(0, 0, 320*2, 300)
+    window.world = jaws.Rect(0, 0, 320*10, 320*2)
 
     for position in [0..20]
       do (position) ->
-        blocks.push(new jaws.Sprite(image: 'img/block.png', x: 0, y: position*32))
-        blocks.push(new jaws.Sprite(image: 'img/block.png', x: world.width-32, y: position*32))
-        blocks.push(new jaws.Sprite(image: 'img/block.png', x: position*32, y: world.height-32))
+        newblock(0, position*32)
+        newblock(position * 32, world.height - 32)
+        newblock(position * 32 + 320, world.height - 64)
 
     tile_map = new jaws.TileMap(size: [100, 100], cell_size: [32, 32])
     tile_map.push(blocks)
@@ -27,16 +38,75 @@ playGameState = ->
 
     window.viewport = new jaws.Viewport(max_x: world.width, max_y: world.height)
 
+    player.move = ->
+      @x += @vx
+      if tile_map.atRect(player.rect()).length > 0
+        @x -= @vx
+      @vx = 0
+
+      @y += @vy
+      block = tile_map.atRect(player.rect())[0]
+      if block?
+        # Heading downwards
+        if @vy > 0
+          @can_jump = true
+          @y = block.rect().y - 1
+        # Heading upwards
+        else if @vy < 0
+          @y = block.rect().bottom + @height
+        @vy = 0
+
+    anim = new jaws.Animation(
+      sprite_sheet: 'img/droid_11x15.png'
+      frame_size: [11,15]
+      frame_duration: 100
+    )
+    player.anim_default = anim.slice(0,5)
+    player.anim_up = anim.slice(6,8)
+    player.anim_down = anim.slice(8,10)
+    player.anim_left = anim.slice(10,12)
+    player.anim_right = anim.slice(12,14)
+    player.vx = player.vy = 0
+    player.can_jump = true
+
+    player.setImage player.anim_default.next()
+
     jaws.context.mozImageSmoothingEnabled = false
     jaws.preventDefaultKeys(["up", "down", "left", "right", "space"])
     console.log 'Setup!'
+    return
   update: ->
+    player.setImage player.anim_default.next()
+    player.vx = 0
+
+    if jaws.pressed('left')
+      player.vx = -2
+      player.setImage player.anim_left.next()
+
+    if jaws.pressed('right')
+      player.vx = 2
+      player.setImage player.anim_right.next()
+
+    if jaws.pressed('up') && player.can_jump
+      player.vy = -10
+      player.can_jump = false
+ 
+    player.vy += 0.4
+    player.move()
+
+    # Center the viewport:
+    viewport.centerAround player
     fps.text jaws.game_loop.fps
+    return
+
   draw: ->
     jaws.clear()
 
     viewport.apply ->
       blocks.draw()
+      player.draw()
+      return
+    return
 
 $ ->
   jaws.assets.add ['img/block.png', 'img/droid_11x15.png']
--- js/tinybountyhunters.js
+++ js/tinybountyhunters.js
@@ -6,7 +6,7 @@
   menuGameState = function() {
     return {
       setup: function() {
-        return jaws.on_keydown(['enter', 'space'], function() {
+        jaws.on_keydown(['enter', 'space'], function() {
           return jaws.switchGameState(playGameState);
         });
       },
@@ -15,33 +15,35 @@
         jaws.context.font = 'bold 50pt Terminal';
         jaws.context.fillStyle = "Black";
         jaws.context.strokeStyle = "rgba(200,200,200,0.0)";
-        return jaws.context.fillText('Start', 30, 160);
+        jaws.context.fillText('Start', 30, 160);
       }
     };
   };
 
   playGameState = function() {
+    var player;
+    player = new jaws.Sprite({
+      x: 110,
+      y: 64,
+      scale: 2,
+      anchor: 'center_bottom'
+    });
     return {
       setup: function() {
-        var position, tile_map, _fn;
-        window.blocks = new jaws.SpriteList();
-        window.world = jaws.Rect(0, 0, 320 * 2, 300);
-        _fn = function(position) {
-          blocks.push(new jaws.Sprite({
-            image: 'img/block.png',
-            x: 0,
-            y: position * 32
-          }));
-          blocks.push(new jaws.Sprite({
-            image: 'img/block.png',
-            x: world.width - 32,
-            y: position * 32
-          }));
+        var anim, newblock, position, tile_map, _fn;
+        newblock = function(x, y) {
           return blocks.push(new jaws.Sprite({
             image: 'img/block.png',
-            x: position * 32,
-            y: world.height - 32
+            x: x,
+            y: y
           }));
+        };
+        window.blocks = new jaws.SpriteList();
+        window.world = jaws.Rect(0, 0, 320 * 10, 320 * 2);
+        _fn = function(position) {
+          newblock(0, position * 32);
+          newblock(position * 32, world.height - 32);
+          return newblock(position * 32 + 320, world.height - 64);
         };
         for (position = 0; position <= 20; position++) {
           _fn(position);
@@ -56,17 +58,65 @@
           max_x: world.width,
           max_y: world.height
         });
+        player.move = function() {
+          var block;
+          this.x += this.vx;
+          if (tile_map.atRect(player.rect()).length > 0) this.x -= this.vx;
+          this.vx = 0;
+          this.y += this.vy;
+          block = tile_map.atRect(player.rect())[0];
+          if (block != null) {
+            if (this.vy > 0) {
+              this.can_jump = true;
+              this.y = block.rect().y - 1;
+            } else if (this.vy < 0) {
+              this.y = block.rect().bottom + this.height;
+            }
+            return this.vy = 0;
+          }
+        };
+        anim = new jaws.Animation({
+          sprite_sheet: 'img/droid_11x15.png',
+          frame_size: [11, 15],
+          frame_duration: 100
+        });
+        player.anim_default = anim.slice(0, 5);
+        player.anim_up = anim.slice(6, 8);
+        player.anim_down = anim.slice(8, 10);
+        player.anim_left = anim.slice(10, 12);
+        player.anim_right = anim.slice(12, 14);
+        player.vx = player.vy = 0;
+        player.can_jump = true;
+        player.setImage(player.anim_default.next());
         jaws.context.mozImageSmoothingEnabled = false;
         jaws.preventDefaultKeys(["up", "down", "left", "right", "space"]);
-        return console.log('Setup!');
+        console.log('Setup!');
       },
       update: function() {
-        return fps.text(jaws.game_loop.fps);
+        player.setImage(player.anim_default.next());
+        player.vx = 0;
+        if (jaws.pressed('left')) {
+          player.vx = -2;
+          player.setImage(player.anim_left.next());
+        }
+        if (jaws.pressed('right')) {
+          player.vx = 2;
+          player.setImage(player.anim_right.next());
+        }
+        if (jaws.pressed('up') && player.can_jump) {
+          player.vy = -10;
+          player.can_jump = false;
+        }
+        player.vy += 0.4;
+        player.move();
+        viewport.centerAround(player);
+        fps.text(jaws.game_loop.fps);
       },
       draw: function() {
         jaws.clear();
-        return viewport.apply(function() {
-          return blocks.draw();
+        viewport.apply(function() {
+          blocks.draw();
+          player.draw();
         });
       }
     };
